aeef0b67fd0c1ce8bd22f942c6fdbc93853b533a
LUIZ CARLOS PEDRO <luiz_c_pedro@estudante.sc.senai.br>
2024-08-19 21:43:00 -0300

    subindo os arq

---
+++ semana01/src/index.ts
@@ -0,0 +1,45 @@
+import leia from 'readline-sync'
+
+/*Escreva um algoritmo que receba 5 nomes e 5 idades do usuário,
+ armazene essas informações em dois vetores, e depois exiba o nome e a idade da pessoa mais velha e da pessoa mais nova.*/
+var idadeMenor : number = 0;
+var nomeMenor: string;
+var idadeMaior: number = 0;
+var nomeMaior: string;
+var vetorIdades: number[] = [];
+var vetorNomes : string[] = [];
+
+function pedirNomes(){
+
+    for(var i = 0; i < 3; i++){
+        var nomeUser: string = leia.question('digite um nome: ');
+        vetorNomes.push(nomeUser);
+        var idadeUser: number = leia.questionInt('digite um numero: ');
+        vetorIdades.push(idadeUser)
+    }
+    
+}   
+
+function verificarMenorIdade(){
+    for(var i = 0; i < vetorIdades.length; i++){
+        if(!idadeMenor || vetorIdades[i] < idadeMenor){
+            idadeMenor = vetorIdades[i];
+            nomeMenor = vetorNomes[i];
+        }
+    }
+    console.log(`a maior idade é: ${idadeMenor}, do usuario ${nomeMenor}`);
+}
+
+function verificarMaiorIdade(){
+    for(var j = 0; j < vetorIdades.length; j++){
+        if(!idadeMaior || vetorIdades[j] > idadeMaior){
+            idadeMaior = vetorIdades[j];
+            nomeMaior = vetorNomes[j]
+        }
+    }
+    console.log(`a maior idade é: ${idadeMaior}, do usuario ${nomeMaior}`);
+}
+
+pedirNomes();
+verificarMenorIdade();
+verificarMaiorIdade(); 
