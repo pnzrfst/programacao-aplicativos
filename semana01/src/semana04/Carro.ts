277a4e3d0f0a5faab82c6d7d1e50f25e9a12f6a2
pnzrfst <jxssxp@gmail.com>
2024-09-10 18:11:30 -0300

    feat: começando a concessionaria

--- semana01/src/semana04/Carro.ts
+++ semana01/src/semana04/Carro.ts
@@ -1,12 +1,25 @@
 import Veiculo from "./Veiculo";
 
 export default class Carro extends Veiculo{
-    private numeroPortas : number;
-    constructor(marca: string, modelo: string, ano: number){
-        super(marca, modelo, ano);
-        this.numeroPortas = 4;
-    }
+    protected numeroPortas : number;
+    protected classificacao : string;
 
+    constructor(marca: string, modelo: string, ano: number, numeroPortas: number, classificacao: string){
+        super(marca, modelo, ano);
+        this.numeroPortas = numeroPortas;
+        this.classificacao = classificacao;
+    }
     
+    public override verDetalhes(): void {
+        console.log(`
+            Marca: ${this.marca},
+            Modelo: ${this.modelo},
+            Nº de portas: ${this.numeroPortas}
+            Classificacao: ${this.classificacao}
+            Ano: ${this.ano},
+            Disponivel: ${this.estaDisponivel}
+            Historico de transferencias: ${this.histTransf}
+            `)
+    }
     
 }
